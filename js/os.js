0e7e355ae10e3e9bc7191d2dc5d33bd9297eebe0
Steve Sarmiento <37785793+stvsrm@users.noreply.github.com>
2025-02-02 12:56:45 -0700

    small changes

--- js/os.js
+++ js/os.js
@@ -143,179 +143,179 @@ makeDraggable(document.getElementById("window-about"));
 
 
 // DRAG AND CLOSE WINDOWS
-function makeDraggable(windowElement) {
-	let pos1 = 0,
-		pos2 = 0,
-		pos3 = 0,
-		pos4 = 0;
+// function makeDraggable(windowElement) {
+// 	let pos1 = 0,
+// 		pos2 = 0,
+// 		pos3 = 0,
+// 		pos4 = 0;
 
-	let windowDragArea = windowElement.getElementsByClassName("window-drag-area")[0];
-	let windowClose = windowElement.getElementsByClassName("window-close")[0];
+// 	let windowDragArea = windowElement.getElementsByClassName("window-drag-area")[0];
+// 	let windowClose = windowElement.getElementsByClassName("window-close")[0];
 
-	//Hide Window if [X] clicked/tapped
-	windowClose.onmouseup = function () { hideWindow(windowClose) };
-	windowClose.ontouchcancel = function () { hideWindow(windowClose) };
-	function hideWindow() {
-		console.log("close touched");
-		windowClose.parentElement.parentElement.classList.toggle("display-block");
-	}
-	windowClose.ontouchstart = function () { hideWindow(windowClose) };
-	function hideWindow() {
-		console.log("close touched");
-		windowClose.parentElement.parentElement.classList.toggle("display-block");
-	}
+// 	//Hide Window if [X] clicked/tapped
+// 	windowClose.onmouseup = function () { hideWindow(windowClose) };
+// 	windowClose.ontouchcancel = function () { hideWindow(windowClose) };
+// 	function hideWindow() {
+// 		console.log("close touched");
+// 		windowClose.parentElement.parentElement.classList.toggle("display-block");
+// 	}
+// 	windowClose.ontouchstart = function () { hideWindow(windowClose) };
+// 	function hideWindow() {
+// 		console.log("close touched");
+// 		windowClose.parentElement.parentElement.classList.toggle("display-block");
+// 	}
 
-	//Bring window to front
-	windowElement.onmousedown = function () { bringWindowToFront(windowElement) };
-	function bringWindowToFront() {
-		windowElement.parentNode.appendChild(windowElement);
-	}
-	windowElement.ontouchstart = function () { bringWindowToFront(windowElement) };
-	function bringWindowToFront() {
-		windowElement.parentNode.appendChild(windowElement);
-	}
+// 	//Bring window to front
+// 	windowElement.onmousedown = function () { bringWindowToFront(windowElement) };
+// 	function bringWindowToFront() {
+// 		windowElement.parentNode.appendChild(windowElement);
+// 	}
+// 	windowElement.ontouchstart = function () { bringWindowToFront(windowElement) };
+// 	function bringWindowToFront() {
+// 		windowElement.parentNode.appendChild(windowElement);
+// 	}
 
-	//Window Drag Area
-	if (windowDragArea !== undefined) {
-		// if there is header, the header is where you move the DIV from
-		windowDragArea.onmousedown = dragMouseDown;
-		windowDragArea.addEventListener("touchstart", dragMouseDown, false); //added touch event
-	} else {
-		// otherwise, move the DIV from anywhere inside the DIV
-		windowElement.onmousedown = dragMouseDown;
-		windowElement.addEventListener("touchstart", dragMouseDown, false); //added touch event
-	}
+// 	//Window Drag Area
+// 	if (windowDragArea !== undefined) {
+// 		// if there is header, the header is where you move the DIV from
+// 		windowDragArea.onmousedown = dragMouseDown;
+// 		windowDragArea.addEventListener("touchstart", dragMouseDown, false); //added touch event
+// 	} else {
+// 		// otherwise, move the DIV from anywhere inside the DIV
+// 		windowElement.onmousedown = dragMouseDown;
+// 		windowElement.addEventListener("touchstart", dragMouseDown, false); //added touch event
+// 	}
 
-	function dragMouseDown(e) {
-		//Bring window to front
-		if (!e.target.matches('.window-close')) {
-			windowElement.parentNode.appendChild(windowElement);
-		}
+// 	function dragMouseDown(e) {
+// 		//Bring window to front
+// 		if (!e.target.matches('.window-close')) {
+// 			windowElement.parentNode.appendChild(windowElement);
+// 		}
 
 
-		console.log("dragMouseDown called by event: ", e.type);
-		let x;
-		let y;
+// 		console.log("dragMouseDown called by event: ", e.type);
+// 		let x;
+// 		let y;
 
-		//Get touch or click position
-		//https://stackoverflow.com/a/41993300/5078983
-		if (
-			e.type === "touchstart" ||
-			e.type === "touchmove" ||
-			e.type === "touchend" ||
-			e.type === "touchcancel"
-		)
-		{
-			let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
-			let touch = evt.touches[0] || evt.changedTouches[0];
-			x = touch.pageX;
-			y = touch.pageY;
-		} else if (
-			e.type === "mousedown" ||
-			e.type === "mouseup" ||
-			e.type === "mousemove" ||
-			e.type === "mouseover" ||
-			e.type === "mouseout" ||
-			e.type === "mouseenter" ||
-			e.type === "mouseleave"
-		) {
-			x = e.clientX;
-			y = e.clientY;
-		}
+// 		//Get touch or click position
+// 		//https://stackoverflow.com/a/41993300/5078983
+// 		if (
+// 			e.type === "touchstart" ||
+// 			e.type === "touchmove" ||
+// 			e.type === "touchend" ||
+// 			e.type === "touchcancel"
+// 		)
+// 		{
+// 			let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
+// 			let touch = evt.touches[0] || evt.changedTouches[0];
+// 			x = touch.pageX;
+// 			y = touch.pageY;
+// 		} else if (
+// 			e.type === "mousedown" ||
+// 			e.type === "mouseup" ||
+// 			e.type === "mousemove" ||
+// 			e.type === "mouseover" ||
+// 			e.type === "mouseout" ||
+// 			e.type === "mouseenter" ||
+// 			e.type === "mouseleave"
+// 		) {
+// 			x = e.clientX;
+// 			y = e.clientY;
+// 		}
 
-		console.log("drag start x: " + x + " y:" + y);
+// 		console.log("drag start x: " + x + " y:" + y);
 
-		// get the mouse cursor position at startup:
-		pos3 = x;
-		pos4 = y;
-		document.onmouseup = closeDragElement;
-		// document.ontouchend = closeDragElement;
-		document.addEventListener("touchend", closeDragElement, false);
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag;
-		// document.ontouchmove = elementDrag;
-		document.addEventListener("touchmove", elementDrag, false);
-	}
+// 		// get the mouse cursor position at startup:
+// 		pos3 = x;
+// 		pos4 = y;
+// 		document.onmouseup = closeDragElement;
+// 		// document.ontouchend = closeDragElement;
+// 		document.addEventListener("touchend", closeDragElement, false);
+// 		// call a function whenever the cursor moves:
+// 		document.onmousemove = elementDrag;
+// 		// document.ontouchmove = elementDrag;
+// 		document.addEventListener("touchmove", elementDrag, false);
+// 	}
 
-	function elementDrag(e) {
-		let x;
-		let y;
+// 	function elementDrag(e) {
+// 		let x;
+// 		let y;
 
-		//Get touch or click position
-		//https://stackoverflow.com/a/41993300/5078983
-		if (
-			e.type === "touchstart" ||
-			e.type === "touchmove" ||
-			e.type === "touchend" ||
-			e.type === "touchcancel"
-		) {
-			let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
-			let touch = evt.touches[0] || evt.changedTouches[0];
-			x = touch.pageX;
-			y = touch.pageY;
-		} else if (
-			e.type === "mousedown" ||
-			e.type === "mouseup" ||
-			e.type === "mousemove" ||
-			e.type === "mouseover" ||
-			e.type === "mouseout" ||
-			e.type === "mouseenter" ||
-			e.type === "mouseleave"
-		) {
-			x = e.clientX;
-			y = e.clientY;
-		}
+// 		//Get touch or click position
+// 		//https://stackoverflow.com/a/41993300/5078983
+// 		if (
+// 			e.type === "touchstart" ||
+// 			e.type === "touchmove" ||
+// 			e.type === "touchend" ||
+// 			e.type === "touchcancel"
+// 		) {
+// 			let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
+// 			let touch = evt.touches[0] || evt.changedTouches[0];
+// 			x = touch.pageX;
+// 			y = touch.pageY;
+// 		} else if (
+// 			e.type === "mousedown" ||
+// 			e.type === "mouseup" ||
+// 			e.type === "mousemove" ||
+// 			e.type === "mouseover" ||
+// 			e.type === "mouseout" ||
+// 			e.type === "mouseenter" ||
+// 			e.type === "mouseleave"
+// 		) {
+// 			x = e.clientX;
+// 			y = e.clientY;
+// 		}
 
-		// calculate the new cursor position:
-		pos1 = pos3 - x;
-		pos2 = pos4 - y;
-		pos3 = x;
-		pos4 = y;
+// 		// calculate the new cursor position:
+// 		pos1 = pos3 - x;
+// 		pos2 = pos4 - y;
+// 		pos3 = x;
+// 		pos4 = y;
 
-		// Restrict movable area
-		let container = document.getElementById("main");
+// 		// Restrict movable area
+// 		let container = document.getElementById("main");
 
-		var windowInfo = windowElement.getBoundingClientRect();
-		var windowWidth = windowInfo.width;
-		var windowHeight = windowInfo.height;
+// 		var windowInfo = windowElement.getBoundingClientRect();
+// 		var windowWidth = windowInfo.width;
+// 		var windowHeight = windowInfo.height;
 
-		var containerInfo = container.getBoundingClientRect();
-		var containerWidth = containerInfo.width;
-		var containerHeight = containerInfo.height;
+// 		var containerInfo = container.getBoundingClientRect();
+// 		var containerWidth = containerInfo.width;
+// 		var containerHeight = containerInfo.height;
 
-		newPosTop = windowElement.offsetTop - pos2;
-		newPosLeft = windowElement.offsetLeft - pos1;
+// 		newPosTop = windowElement.offsetTop - pos2;
+// 		newPosLeft = windowElement.offsetLeft - pos1;
 
-		if (newPosTop < windowHeight / 2) {
-			newPosTop = windowHeight / 2;
-		}
-		if (newPosTop > containerHeight - (windowHeight / 2)) {
-			newPosTop = containerHeight - (windowHeight / 2);
-		}
-		if (newPosLeft < windowWidth / 2) {
-			newPosLeft = windowWidth / 2;
-		}
-		if (newPosLeft > containerWidth - (windowWidth / 2)) {
-			newPosLeft = containerWidth - (windowWidth / 2);
-		}
+// 		if (newPosTop < windowHeight / 2) {
+// 			newPosTop = windowHeight / 2;
+// 		}
+// 		if (newPosTop > containerHeight - (windowHeight / 2)) {
+// 			newPosTop = containerHeight - (windowHeight / 2);
+// 		}
+// 		if (newPosLeft < windowWidth / 2) {
+// 			newPosLeft = windowWidth / 2;
+// 		}
+// 		if (newPosLeft > containerWidth - (windowWidth / 2)) {
+// 			newPosLeft = containerWidth - (windowWidth / 2);
+// 		}
 
-		// set the element's new position:
-		windowElement.style.top = newPosTop + "px";
-		windowElement.style.left = newPosLeft + "px";
-	}
+// 		// set the element's new position:
+// 		windowElement.style.top = newPosTop + "px";
+// 		windowElement.style.left = newPosLeft + "px";
+// 	}
 
-	function closeDragElement() {
-		console.log("drag end x: " + pos3 + " y:" + pos4);
-		// stop moving when mouse button is released:
-		document.onmouseup = null;
-		document.ontouchcancel = null; //added touch event
-		document.onmousemove = null;
-		// document.ontouchend = null; //added touch event
-		document.removeEventListener("touchend", closeDragElement, false);
-		document.removeEventListener("touchmove", elementDrag, false);
-		// document.ontouchmove = null; //added touch event
-	}
-}
+// 	function closeDragElement() {
+// 		console.log("drag end x: " + pos3 + " y:" + pos4);
+// 		// stop moving when mouse button is released:
+// 		document.onmouseup = null;
+// 		document.ontouchcancel = null; //added touch event
+// 		document.onmousemove = null;
+// 		// document.ontouchend = null; //added touch event
+// 		document.removeEventListener("touchend", closeDragElement, false);
+// 		document.removeEventListener("touchmove", elementDrag, false);
+// 		// document.ontouchmove = null; //added touch event
+// 	}
+// }
 
 
 
